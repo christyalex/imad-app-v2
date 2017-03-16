521a7382dc85d3210cb6eb02bafca204ded55eed
christyalex <christyalexe@gmail.com>
2017-03-16 11:36:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,9 +8,11 @@ app.get('/article-one',function(req,res){
      res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 app.get('/article-two',function(req,res){
-    res.send('this is my second artcle')});
+    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+});
 app.get('/article-three',function(req,res){
-    res.send('this is my third artcle')});
+   res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+});
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
