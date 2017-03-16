29a0cc4666beebe00ba7b8a59ad42c064999c202
christyalex <christyalexe@gmail.com>
2017-03-17 02:00:07 +0530

    [imad-console] Updates ui/my.js

--- ui/my.js
+++ ui/my.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
 var app = express();
 app.use(morgan('combined'));
 var counter=0;
 app.get('/counter',function(req,res){
     counter=counter+1;
-    res.send(counter.tostring());
+    res.send(counter.toString());
+});
     app.get('/article-two',function(req,res){
     res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
